1060ae866bfe3dfdbf5e370789b80d9dbfb3b00e
Diego <diego.galvez.vargas@gmail.com>
2023-12-20 17:32:36 -0300

    prueba inicial

---
+++ part1/1.12/anecdotes/src/components/Course.js
@@ -0,0 +1,20 @@
+// Course.js
+
+import React from 'react';
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <ul>
+        {course.parts.map(part => (
+          <li key={part.id}>
+            {part.name} - {part.exercises} exercises
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Course;
