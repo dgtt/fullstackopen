7135e160e0848c0d1be1689fff31c8445bbbe7a9
Diego <diego.galvez.vargas@gmail.com>
2024-01-08 17:27:01 -0300

    Ejercicios 4.1 al 4.7

---
+++ part4/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+        'jest': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+        'config.js'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': 0,
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
